d9c4c1ee1bb10da512ad2beab9d4047ddde2a538
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-02-06 18:43:49 +0100

    Improved accessibility of the entry boxes.

--- main.py
+++ main.py
@@ -24,11 +24,19 @@ class GatewayWindow(gtk.Window):
         table = gtk.Table(rows=3, columns=2)
         self.add(table)
 
-        username_label = gtk.Label("Username")
+        username_entry = gtk.Entry()
+        username_label = gtk.Label("_Username:")
+        username_label.set_use_underline(True)
+        username_label.set_mnemonic_widget(username_entry)
+        table.attach(username_entry, 1, 2, 0, 1)
         table.attach(username_label, 0, 1, 0, 1)
 
-        username_entry = gtk.Entry()
-        table.attach(username_entry, 1, 2, 0, 1)
+        password_entry = gtk.Entry()
+        password_label = gtk.Label("_Password:")
+        password_label.set_use_underline(True)
+        password_label.set_mnemonic_widget(password_entry)
+        table.attach(password_entry, 1, 2, 1, 2)
+        table.attach(password_label, 0, 1, 1, 2)
 
         table.show_all()
 
