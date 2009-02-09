a61a7c28550fa134e9d659425cb48aeb9f0ae53d
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-02-09 15:08:09 +0100

    Made the application actually work.

--- gateway.py
+++ gateway.py
@@ -20,6 +20,10 @@ class Agent:
         client = self.client
         try:
             client.connect(self.host, self.port, username, password, look_for_keys=False)
+            channel = client.invoke_shell()
+            print channel.recv(1024)
+            print channel.recv(1024)
+            channel.close()
             client.close()
         except paramiko.AuthenticationException:
             raise AuthenticationException()
