72ec24798617c40e438f380029224dea08c18630
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-02-10 18:12:55 +0100

    Fixed up the build system.

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='Egmont Connect',
+setup(name='egmont-gateway',
       version='0.1',
       description='Connect to the Egmont network',
       author='Daniel Schierbeck',
       author_email='daniel.schierbeck@gmail.com',
       url='http://github.com/dasch/egmont-gateway',
       packages=['egmont'],
-      data_files=[('share', 'data/egmont.glade')],
+      data_files=[('share/egmont-gateway/glade', ['data/egmont.glade'])],
+      scripts=['egmont-gateway'],
      )
 
