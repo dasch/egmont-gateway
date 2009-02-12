f943a4e59215d04eb0156dd9cf983b41776cb11f
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-02-13 00:05:55 +0100

    Bumped version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='egmont-gateway',
-      version='0.3',
+      version='0.4',
       description='Connect to the Egmont network',
       author='Daniel Schierbeck',
       author_email='daniel.schierbeck@gmail.com',
