6c73819e33e3812a860f4a470b30bedf3017a000
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-02-12 22:26:55 +0100

    Refactored a bit.

--- egmont/gateway.py
+++ egmont/gateway.py
@@ -2,42 +2,37 @@
 import paramiko
 import socket
 
-HOST = "bifrost.egmont-kol.dk"
-PORT = 22
 
 class AuthenticationException(Exception):
     pass
 
+
 class NetworkError(Exception):
     pass
+
 
 class AccountClosedException(Exception):
     pass
 
-class Agent:
 
-    def __init__(self, host=HOST, port=PORT):
-        self.host = host
-        self.port = port
-        self.client = paramiko.SSHClient()
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def connect(host, port, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    def connect(self, username, password):
-        client = self.client
+    try:
+        client.connect(host, port, username, password, look_for_keys=False)
         try:
-            client.connect(self.host, self.port, username, password, look_for_keys=False)
-            try:
-                channel = client.invoke_shell()
-                channel.recv(1024)
-                response = channel.recv(1024)
+            channel = client.invoke_shell()
+            channel.recv(1024)
+            response = channel.recv(1024)
 
-                if response == "This machine or user is closed.":
-                    raise AccountClosedException()
-            finally:
-                channel.close()
-        except paramiko.AuthenticationException:
-            raise AuthenticationException()
-        except socket.error:
-            raise NetworkError()
+            if response == "This machine or user is closed.":
+                raise AccountClosedException()
         finally:
-            client.close()
+            channel.close()
+    except paramiko.AuthenticationException:
+        raise AuthenticationException()
+    except socket.error:
+        raise NetworkError()
+    finally:
+        client.close()
